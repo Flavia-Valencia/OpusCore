51789425f983aff48b7a50401c76da6c43727b99
Key <keysanchez706@gmail.com>
2026-03-02 15:13:47 -0600

    login

--- login.php
+++ login.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesLogin.css">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Hola Mundo</h1>
+    <h2>Iniciar Sesión</h2>
+    <form method="POST">
+        <input type="text" name="usuario" placeholder="Usuario" required><br><br>
+        <input type="password" name="password" placeholder="Contraseña" required><br><br>
+        <button type="submit">Entrar</button>
+    </form>
 </body>
 </html>
