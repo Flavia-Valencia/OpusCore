0f95205339aaffc3381d5d2440d492aed365a1c9
Flavia <yasuriyamileth2704@gmail.com>
2026-03-04 16:37:14 -0600

    Arregle Logout

--- includes/logout.php
+++ includes/logout.php
@@ -1,5 +1,14 @@
 <?php #cree este archivo para que cuando el usuario le de click al icono de cerrar sesion(logout) se salga y lo redirija al login.php
 session_start();
+
+$_SESSION = [];
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 session_destroy();
 header("Location: ../login.php");
 exit();
