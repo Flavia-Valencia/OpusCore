47e367e3df4060d94d81eb217daaa8a6d1702d41
Key <keysanchez706@gmail.com>
2026-03-04 11:43:53 -0600

    nada

--- admin-inicio.php
+++ admin-inicio.php
@@ -7,6 +7,7 @@
     <link rel="icon" type="image/svg+xml" href="img/logo.svg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/stylesAdmin.css">
+    
 </head>
 <body>
    
